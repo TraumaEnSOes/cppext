6941588842ebd9b9558946fb17dbfa94f675c4cd
TraumaEnSOes <traumaensoes@gmail.com>
2019-07-19 08:36:10 +0200

    Optimización

--- signal.hpp
+++ signal.hpp
@@ -25,62 +25,39 @@ public:
   bool empty( ) const { return m_vector.empty( ); }
   size_type size( ) const { return m_vector.size( ); }
   void clear( ) { m_vector.clear( ); }
+
+  template< typename... ARGS > void connect( void ( *target )( ARGS... ) ) {
+        m_vector.emplace_back( reinterpret_cast< void * >( target ) );
+  }
+  template< typename CLASS, typename... ARGS > void connect( CLASS *self, void ( CLASS::*method )( ARGS... ) ) {
+    auto lambda = []( Target &target, ARGS... args ) {
+      auto tself = reinterpret_cast< CLASS * >( target.self );
+      auto tmethod = reinterpret_cast< void ( CLASS::* )( ARGS... ) >( target.method );
+
+      ( tself->*tmethod )( std::forward< ARGS >( args )... );
+    };
+
+    m_vector.emplace_back( reinterpret_cast< void * >( *lambda ), reinterpret_cast< void * >( self ), reinterpret_cast< void ( CLASS::* )( ) >( method ) );
+  }
+  template< typename... ARGS > void operator()( ARGS... args ) {
+    for( auto &iter: m_vector ) {
+      if( iter.self ) {
+        reinterpret_cast< void ( * )( Target &, ARGS... ) >( iter.callback )( iter, std::forward< ARGS >( args )... );
+      } else {
+        reinterpret_cast< void( * )( ARGS... ) >( iter.callback )( std::forward< ARGS >( args )... );
+      }
+    }    
+  }
 };
 
 template< typename FIRST = void, typename... ARGS > class Signal : public SignalBase {
 public:
   using SignalBase::size_type;
-
-  void connect( void( *target )( FIRST, ARGS... ) ) {
-    m_vector.emplace_back( reinterpret_cast< void * >( target ) );
-  }
-  template< typename CLASS > void connect( CLASS *self, void ( CLASS::*method )( FIRST, ARGS... ) ) {
-    auto lambda = []( Target &target, FIRST f, ARGS... args ) {
-      auto tself = reinterpret_cast< CLASS * >( target.self );
-      auto tmethod = reinterpret_cast< void ( CLASS::* )( FIRST, ARGS... ) >( target.method );
-
-      ( tself->*tmethod )( std::forward< FIRST >( f ), std::forward< ARGS >( args )... );
-    };
-
-    m_vector.emplace_back( reinterpret_cast< void * >( *lambda ), reinterpret_cast< void * >( self ), reinterpret_cast< void ( CLASS::* )( ) >( method ) );
-  }
-  void operator()( FIRST &&f, ARGS&&... args ) {
-    for( auto &iter: m_vector ) {
-      if( iter.self ) {
-        reinterpret_cast< void ( * )( Target &, FIRST, ARGS... ) >( iter.callback )( iter, std::forward< FIRST >( f ), std::forward< ARGS >( args )... );
-      } else {
-        reinterpret_cast< void( * )( FIRST, ARGS... ) >( iter.callback )( std::forward<FIRST>( f ), std::forward< ARGS >( args )... );
-      }
-    }
-  }
 };
 
 template< > class Signal< void > : public SignalBase {
 public:
   using SignalBase::size_type;
-
-  void connect( void( *target )( ) ) {
-    m_vector.emplace_back( reinterpret_cast< void * >( target ) );
-  }
-  template< typename CLASS > void connect( CLASS *self, void ( CLASS::*method )( ) ) {
-    auto lambda = []( Target &target ) {
-      auto tself = reinterpret_cast< CLASS * >( target.self );
-      auto tmethod = reinterpret_cast< void ( CLASS::* )( ) >( target.method );
-
-      ( tself->*tmethod )( );
-    };
-
-    m_vector.emplace_back( reinterpret_cast< void * >( *lambda ), reinterpret_cast< void * >( self ), reinterpret_cast< void ( CLASS::* )( ) >( method ) );    
-  }
-  void operator()( ) {
-    for( auto &iter: m_vector ) {
-      if( iter.self ) {
-        reinterpret_cast< void ( * )( Target & ) >( iter.callback )( iter );
-      } else {
-        reinterpret_cast< void( * )( ) >( iter.callback )( );
-      }
-    }
-  }
 };
 
 #endif
